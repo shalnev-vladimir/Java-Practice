0ede840add65bf6ca4fed373e5bd2a6a175a1a69
vladimirshalnev <vladimirs@dmvdesk.com>
2023-01-04 13:54:32 +0300

    added id for each FootballPlayer

--- src/vova/collection/map_interface/HashCodeExample.java
+++ src/vova/collection/map_interface/HashCodeExample.java
@@ -9,19 +9,19 @@ import static java.lang.System.*;
 public class HashCodeExample {
     public static void main(String[] args) {
         Map<FootballPlayer, Integer> footballerAndNumberOfGoalsScored = new HashMap<>();
-        FootballPlayer messi = new FootballPlayer("Lionel", "Messi", "PSG");
-        FootballPlayer ronaldo = new FootballPlayer("Cristiano", "Ronaldo", "Al Nasr");
-        FootballPlayer neymar = new FootballPlayer("Neymar", "Santos", "PSG");
+        FootballPlayer messi = new FootballPlayer(1,"Lionel", "Messi", "PSG");
+        FootballPlayer ronaldo = new FootballPlayer(2, "Cristiano", "Ronaldo", "Al Nasr");
+        FootballPlayer neymar = new FootballPlayer(3, "Neymar", "Santos", "PSG");
         footballerAndNumberOfGoalsScored.put(messi, 11);
         footballerAndNumberOfGoalsScored.put(ronaldo, 0);
         footballerAndNumberOfGoalsScored.put(neymar, 23);
 
-        FootballPlayer gomez1 = new FootballPlayer("Sergio", "Gomez", "Sevilia");
-        FootballPlayer gomez2 = new FootballPlayer("Sergio", "Gomez", "Sevilia");
+        FootballPlayer gomez1 = new FootballPlayer(4, "Sergio", "Gomez", "Sevilia");
+        FootballPlayer gomez2 = new FootballPlayer(5, "Sergio", "Gomez", "Sevilia");
         footballerAndNumberOfGoalsScored.put(gomez1, 7);
         footballerAndNumberOfGoalsScored.put(gomez2, 7);
 
-        // у обоих Гомесов будет одинаковый хэшкод и equals тоже вернет true. Поэтому они равны
+        // у обоих Гомесов будет одинаковый хэшкод и equals тоже вернет true.
         out.println(gomez1.hashCode());
         out.println(gomez2.hashCode());
         out.println(gomez1.equals(gomez2));
@@ -30,11 +30,13 @@ public class HashCodeExample {
 }
 
 class FootballPlayer {
+    int id;
     String firstName;
     String secondName;
     String team;
 
-    public FootballPlayer(String firstName, String secondName, String team) {
+    public FootballPlayer(int id, String firstName, String secondName, String team) {
+        this.id = id;
         this.firstName = firstName;
         this.secondName = secondName;
         this.team = team;
@@ -43,7 +45,8 @@ class FootballPlayer {
     @Override
     public String toString() {
         return "FootballPlayer{" +
-                "firstName='" + firstName + '\'' +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
                 ", secondName='" + secondName + '\'' +
                 ", team='" + team + '\'' +
                 '}';
@@ -54,12 +57,12 @@ class FootballPlayer {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         FootballPlayer that = (FootballPlayer) o;
-        return Objects.equals(firstName, that.firstName) && Objects.equals(secondName, that.secondName)
+        return id == that.id && Objects.equals(firstName, that.firstName) && Objects.equals(secondName, that.secondName)
                 && Objects.equals(team, that.team);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, secondName, team);
+        return Objects.hash(id, firstName, secondName, team);
     }
 }
